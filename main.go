4dfca5c1e857a8e5e52ee62856ffb9ae1e252969
James Hart <james.elliot.hart@gmail.com>
2020-11-07 11:55:29 -0600

    set up database

--- main.go
+++ main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"github.com/jameselliothart/inventoryservice/database"
 	"net/http"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/jameselliothart/inventoryservice/product"
 )
 
 const apiBasePath = "/api"
 
 func main() {
+	database.SetupDatabase()
 	product.SetupRoutes(apiBasePath)
 	http.ListenAndServe(":5000", nil)
 }
